4a703f3533fefd30ad622b2e2b505aa3961f1e3c
Черадионов Сергей Романович <cheradionovsr@cifka-k.ru>
2026-08-24 14:05:15 +0700

    build: попытка накинуть линтер

---
+++ eslint.config.js
@@ -0,0 +1,101 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const importPlugin = require('eslint-plugin-import');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
+    ],
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      'import/internal-regex': '^@msw',
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: ['element', 'attribute'],
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'fbf/restrict-cross-feature-imports': [
+        'error',
+        {
+          featuresFolderPath: 'app',
+          sharedFolderNames: ['shared'],
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+          pathGroups: [
+            {
+              pattern: '{@taiga-ui/**,@tinkoff/**}',
+              group: 'external',
+              position: 'after',
+            },
+            {
+              pattern: '@msw/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 120,
+        },
+      ],
+      quotes: [
+        'error',
+        'single',
+        {
+          allowTemplateLiterals: true,
+          avoidEscape: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  }
+);
