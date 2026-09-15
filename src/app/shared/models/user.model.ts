747332f2502264c8bedb03528e6520b649515a2d
Sergey <118613573+Rysal228@users.noreply.github.com>
2026-09-15 20:51:08 +0700

    add username to current user model

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -2,6 +2,7 @@ import { UserRole } from '../types/roles.types';
 
 export type CurrentUser = {
   id: string;
+  username: string | null;
   phone: string;
   firstName: string;
   lastName: string;
