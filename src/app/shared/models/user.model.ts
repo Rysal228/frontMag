1e953f30793faa15471d1b8f0ece5262d256664b
Sergey <118613573+Rysal228@users.noreply.github.com>
2026-09-15 21:26:59 +0700

    remove username from current user model

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -2,7 +2,6 @@ import { UserRole } from '../types/roles.types';
 
 export type CurrentUser = {
   id: string;
-  username: string | null;
   phone: string;
   firstName: string;
   lastName: string;
