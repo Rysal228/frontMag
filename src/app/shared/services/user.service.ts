3c748d90d5452fe26c44aeae814f60b8b120a663
Sergey <118613573+Rysal228@users.noreply.github.com>
2026-09-15 20:50:41 +0700

    simplify profile service response

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 import { API_ENDPOINTS } from 'app/shared/consts/urls.const';
 import { CurrentUser } from 'app/shared/models/user.model';
@@ -11,7 +11,9 @@ import { CurrentUser } from 'app/shared/models/user.model';
 export class UserService {
   private readonly http = inject(HttpClient);
 
-  public getProfile(): Observable<{ user: CurrentUser }> {
-    return this.http.get<{ user: CurrentUser }>(API_ENDPOINTS.users.profile);
+  public getProfile(): Observable<CurrentUser> {
+    return this.http.get<{ user: CurrentUser }>(API_ENDPOINTS.users.profile).pipe(
+      map(({ user }) => user),
+    );
   }
 }
