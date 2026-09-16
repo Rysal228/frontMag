9c1ee43615f786586ffeb715f7392efd73fc9604
Черадионов Сергей Романович <cheradionovsr@cifka-k.ru>
2026-09-16 19:17:50 +0700

    feat: добавление рус. тайги

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import ru from '@angular/common/locales/ru';
+import { ApplicationConfig, LOCALE_ID, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { of } from 'rxjs';
 
 import { provideEventPlugins } from '@taiga-ui/event-plugins';
+import { TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE } from '@taiga-ui/i18n';
 
 import { routes } from './app.routes';
 import { initializeApplication } from './shared/initializers/app.initializer';
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
+
+registerLocaleData(ru);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +23,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideEventPlugins(),
     provideAppInitializer(initializeApplication),
+    {
+      provide: LOCALE_ID,
+      useValue: 'ru-RU',
+    },
+    {
+      provide: TUI_LANGUAGE,
+      useValue: of(TUI_RUSSIAN_LANGUAGE),
+    },
   ],
 };
