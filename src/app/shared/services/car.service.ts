7b70f2c5a260105123c535b96d95d9a3f86a9cd6
Sergey <118613573+Rysal228@users.noreply.github.com>
2026-09-17 13:13:22 +0700

    refactor(cars): separate api calls from car data mapping

--- src/app/shared/services/car.service.ts
+++ src/app/shared/services/car.service.ts
@@ -1,62 +1,41 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
-import { API_ENDPOINTS } from 'app/shared/consts/urls.const';
+import { CarApiResponse, CarApiService } from 'app/shared/api/car-api.service';
 import { Car, CarBrand, CarModel, CreateCarRequest } from 'app/shared/models/car.model';
-
-interface CarApiResponse {
-  id: string;
-  brand: number;
-  brandName: string;
-  model: number;
-  modelName: string;
-  year: number;
-  vin: string | null;
-  plate_number: string | null;
-  photo: string | null;
-}
 
 @Injectable({
   providedIn: 'root',
 })
 export class CarService {
-  private readonly http = inject(HttpClient);
+  private readonly carApiService = inject(CarApiService);
 
   public getAll(): Observable<Car[]> {
-    return this.http.get<CarApiResponse[]>(API_ENDPOINTS.cars.list).pipe(
-      map((cars) => cars.map((car) => this.mapCar(car)))
-    );
+    return this.carApiService.getAll().pipe(map((cars) => cars.map((car) => this.mapCar(car))));
   }
 
   public getById(id: string): Observable<Car> {
-    return this.http.get<CarApiResponse>(`${API_ENDPOINTS.cars.list}${id}/`).pipe(map((car) => this.mapCar(car)));
+    return this.carApiService.getById(id).pipe(map((car) => this.mapCar(car)));
   }
 
   public getBrands(): Observable<CarBrand[]> {
-    return this.http.get<CarBrand[]>(API_ENDPOINTS.cars.brands);
+    return this.carApiService.getBrands();
   }
 
   public getModels(brandId: number): Observable<CarModel[]> {
-    const params = new HttpParams().set('brand', brandId);
-
-    return this.http.get<CarModel[]>(API_ENDPOINTS.cars.models, { params });
+    return this.carApiService.getModels(brandId);
   }
 
   public create(request: CreateCarRequest): Observable<Car> {
-    return this.http
-      .post<CarApiResponse>(API_ENDPOINTS.cars.list, this.toFormData(request))
-      .pipe(map((car) => this.mapCar(car)));
+    return this.carApiService.create(this.toFormData(request)).pipe(map((car) => this.mapCar(car)));
   }
 
   public update(id: string, request: CreateCarRequest): Observable<Car> {
-    return this.http
-      .patch<CarApiResponse>(`${API_ENDPOINTS.cars.list}${id}/`, this.toFormData(request))
-      .pipe(map((car) => this.mapCar(car)));
+    return this.carApiService.update(id, this.toFormData(request)).pipe(map((car) => this.mapCar(car)));
   }
 
   public delete(id: string): Observable<void> {
-    return this.http.delete<void>(`${API_ENDPOINTS.cars.list}${id}/`);
+    return this.carApiService.delete(id);
   }
 
   private toFormData(request: CreateCarRequest): FormData {
